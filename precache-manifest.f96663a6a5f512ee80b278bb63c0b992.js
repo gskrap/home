8c413747bd0c03c1b20b9d58a7af31994772a9c8
George Skrapits <gskrap@gmail.com>
2020-01-22 21:22:34 -0500

    Updates

---
+++ precache-manifest.f96663a6a5f512ee80b278bb63c0b992.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a7c8c1dfe8e60219306c8bdcbaa42d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96791f0a19b2ac4ef5aa",
+    "url": "/static/css/2.3fad095e.chunk.css"
+  },
+  {
+    "revision": "34540c2adf31e3873845",
+    "url": "/static/css/main.690463af.chunk.css"
+  },
+  {
+    "revision": "96791f0a19b2ac4ef5aa",
+    "url": "/static/js/2.d6ce5847.chunk.js"
+  },
+  {
+    "revision": "79130442c614f955ef355d0307827ea7",
+    "url": "/static/js/2.d6ce5847.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34540c2adf31e3873845",
+    "url": "/static/js/main.fa4ae2f8.chunk.js"
+  },
+  {
+    "revision": "c8229c007c1b6148d468",
+    "url": "/static/js/runtime-main.85791974.js"
+  },
+  {
+    "revision": "76d6a0843273ed4858443ba43c2ed8b9",
+    "url": "/static/media/Honda_Regular.76d6a084.ttf"
+  },
+  {
+    "revision": "5969c0e125317a0eb59c7dd9c22d1e06",
+    "url": "/static/media/Microsoft_Sans_Serif.5969c0e1.ttf"
+  },
+  {
+    "revision": "b896bc341f3478d6758be908521a54d4",
+    "url": "/static/media/TerminalVector.b896bc34.ttf"
+  },
+  {
+    "revision": "626bb66c474f7c3e37d63f2ce1c988c8",
+    "url": "/static/media/apple.626bb66c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7e986a02c3c777f2b09e22eb42467ffc",
+    "url": "/static/media/resume.7e986a02.pdf"
+  }
+]);
